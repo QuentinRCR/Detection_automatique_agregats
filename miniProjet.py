ba64cda4f8abdb9b04e6bf37d99039b7e1d61bd0
Quentin REY <quentin.rey@etu.emse.fr>
2023-04-25 18:47:29 +0200

    save results to a file

--- miniProjet.py
+++ miniProjet.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.ndimage
 import skimage as ski
 import cv2
 
@@ -12,22 +11,16 @@ def applyCleaningFilter(image,filter):
     return np.maximum(filteredImage,np.zeros(filteredImage.shape)).astype(np.uint8)
 
 #generate the filter to remove the main noises. This filter is the sum of every images
-def generateFilter(listImages,imageShape):
+def generateFilter(inputDirectory,listImages,imageShape):
     filter = np.zeros(imageShape, dtype=int)
     for imageName in listImages:
-        image = ski.io.imread(imageName)[:,:,0]
+        image = ski.io.imread(inputDirectory+imageName)[:,:,0]
         filter += image
-    filter = filter // numberImages
+    filter = filter // len(listImages)
     filter =filter.astype(np.uint8)
     if(not os.path.exists("filters")): #if the folder doesn't exist, create it
         os.mkdir("filters")
     ski.io.imsave("filters/luminosityFilter.bmp",filter,format='bmp')
-
-imageDirectory = "Sous_ensemble_test"
-listImages= [imageDirectory+"/"+nameImage for nameImage in os.listdir(imageDirectory)]
-numberImages = len(listImages)
-testImage= ski.io.imread(listImages[1])[:,:,0] #take only one component
-imageShape = testImage.shape[0:2]
 
 
 def removeElementsTouchingEdge(image):
@@ -67,78 +60,98 @@ def getAreaAndPerimeters(image):
         cv2.putText(imageWithAreaAndPerimeter, f'Perimeter :{perimeter}', (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0, 255, 0), 2)
     return (areas,perimeters,imageWithAreaAndPerimeter)
 
-#if the luminosity filter in not yet saved, create it
-if(not os.path.isfile("filters/luminosityFilter.bmp")):
-    generateFilter(listImages,imageShape)
+imageInputDirectory = "Sous_ensemble_test/"
+imageOutputDirectory = "treatedImage/"
+valuesOutputDirectory = "AreaAndPerimeterValues/"
 
-filters = ski.io.imread("filters/luminosityFilter.bmp") #get filter from saved images
+listOfImageNames = os.listdir(imageInputDirectory)
+imageShape = ski.io.imread(imageInputDirectory+listOfImageNames[0]).shape[0:2]
 
-#print original image to show evolution
-plt.subplot(221,title="original image")
-plt.imshow(testImage,"gray")
-plt.axis("off")
+for imageName in listOfImageNames:
+    if(os.path.isfile(valuesOutputDirectory+imageName[:-4]+".csv")): #it the image was already treated, remove it
+        continue
 
+    print("treating ",imageName)
 
-#apply luminosity fiter and equalize image
-cleanedEqualizedImage = ski.exposure.equalize_hist(applyCleaningFilter(testImage, filters))
-plt.subplot(222,title='cleaned equalized image')
-plt.imshow(cleanedEqualizedImage, "gray")
-plt.axis("off")
+    testImage= ski.io.imread(imageInputDirectory+imageName)[:,:,0] #take only one component
 
-#remove a bit of noise on the image to have a darker background
-morphedImage = cv2.morphologyEx(cleanedEqualizedImage, cv2.MORPH_OPEN, np.ones((4,4), np.uint8))
-plt.subplot(223,title="darker background with morph open")
-plt.imshow(morphedImage,"gray")
-plt.axis("off")
+    #if the luminosity filter in not yet saved, create it
+    if(not os.path.isfile("filters/luminosityFilter.bmp")):
+        generateFilter(imageInputDirectory,listImages,imageShape)
 
-# uniformize blobs to avoid noise and bloobs fusions with filter
-plt.subplot(224,title="blured image")
-bluedImage = ski.filters.gaussian(morphedImage,1)
-plt.imshow(bluedImage,"gray")
-plt.axis("off")
+    filters = ski.io.imread("filters/luminosityFilter.bmp") #get filter from saved images
 
-plt.figure()
-
-#make the image binary with a local threshold
-threshold=ski.filters.threshold_local(bluedImage,301)
-binaryImage = bluedImage>threshold
-plt.subplot(221,title="binary image")
-binaryImage = binaryImage.astype(np.uint8) #transorm it to uint8
-binaryImage[binaryImage==True]=255
-plt.imshow(binaryImage,"gray")
-plt.axis("off")
-
-#remove small noise
-morphedBinaryImage = cv2.morphologyEx(binaryImage, cv2.MORPH_OPEN, np.ones((20, 20), np.uint8))
-plt.subplot(222, title="remove noise")
-plt.imshow(morphedBinaryImage,"gray")
-plt.axis("off")
-
-#smooth image with median filter
-medianImage = ski.filters.median(morphedBinaryImage,ski.morphology.disk(10) )
-plt.subplot(223,title="smoothed image")
-plt.imshow(medianImage,"gray")
-plt.axis("off")
-
-#remove elements touching edges
-cleanEdgeImage =removeElementsTouchingEdge(medianImage)
-plt.subplot(224,title="remove edge elements")
-plt.axis("off")
-plt.imshow(cleanEdgeImage,"gray")
+    #print original image to show evolution
+    plt.subplot(221,title="original image")
+    plt.imshow(testImage,"gray")
+    plt.axis("off")
 
 
-#find perimeters, areas and blobs and display it on the image
-areas,perimeters,imageWithAreaAndPerimeter=getAreaAndPerimeters(cleanEdgeImage)
+    #apply luminosity fiter and equalize image
+    cleanedEqualizedImage = ski.exposure.equalize_hist(applyCleaningFilter(testImage, filters))
+    plt.subplot(222,title='cleaned equalized image')
+    plt.imshow(cleanedEqualizedImage, "gray")
+    plt.axis("off")
 
-plt.figure()
-# plt.title("final comparison")
-plt.subplot(121,title="original image")
-plt.imshow(testImage,"gray")
-plt.axis("off")
+    #remove a bit of noise on the image to have a darker background
+    morphedImage = cv2.morphologyEx(cleanedEqualizedImage, cv2.MORPH_OPEN, np.ones((4,4), np.uint8))
+    plt.subplot(223,title="darker background with morph open")
+    plt.imshow(morphedImage,"gray")
+    plt.axis("off")
 
-plt.subplot(122,title="treated image")
-plt.imshow(imageWithAreaAndPerimeter,"gray")
-plt.axis("off")
+    # uniformize blobs to avoid noise and bloobs fusions with filter
+    plt.subplot(224,title="blured image")
+    bluedImage = ski.filters.gaussian(morphedImage,1)
+    plt.imshow(bluedImage,"gray")
+    plt.axis("off")
+
+    plt.figure()
+
+    #make the image binary with a local threshold
+    threshold=ski.filters.threshold_local(bluedImage,301)
+    binaryImage = bluedImage>threshold
+    plt.subplot(221,title="binary image")
+    binaryImage = binaryImage.astype(np.uint8) #transorm it to uint8
+    binaryImage[binaryImage==True]=255
+    plt.imshow(binaryImage,"gray")
+    plt.axis("off")
+
+    #remove small noise
+    morphedBinaryImage = cv2.morphologyEx(binaryImage, cv2.MORPH_OPEN, np.ones((20, 20), np.uint8))
+    plt.subplot(222, title="remove noise")
+    plt.imshow(morphedBinaryImage,"gray")
+    plt.axis("off")
+
+    #smooth image with median filter
+    medianImage = ski.filters.median(morphedBinaryImage,ski.morphology.disk(10) )
+    plt.subplot(223,title="smoothed image")
+    plt.imshow(medianImage,"gray")
+    plt.axis("off")
+
+    #remove elements touching edges
+    cleanEdgeImage =removeElementsTouchingEdge(medianImage)
+    plt.subplot(224,title="remove edge elements")
+    plt.axis("off")
+    plt.imshow(cleanEdgeImage,"gray")
 
 
-plt.show()
+    #find perimeters, areas and blobs and display it on the image
+    areas,perimeters,imageWithAreaAndPerimeter=getAreaAndPerimeters(cleanEdgeImage)
+
+    plt.imsave(imageOutputDirectory+imageName,imageWithAreaAndPerimeter)
+    with open(valuesOutputDirectory+imageName[:-4]+".csv", 'w') as f:
+        for area,perimeter in zip(areas,perimeters):
+            f.write(str(area)+";"+str(perimeter)+"\n")
+
+    plt.figure()
+    # plt.title("final comparison")
+    plt.subplot(121,title="original image")
+    plt.imshow(testImage,"gray")
+    plt.axis("off")
+
+    plt.subplot(122,title="treated image")
+    plt.imshow(imageWithAreaAndPerimeter,"gray")
+    plt.axis("off")
+
+
+    # plt.show()
