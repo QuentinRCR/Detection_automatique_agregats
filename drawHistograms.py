86438bb0c609087fcf4a18b49f8a9674f21dd92a
Quentin REY <quentin.rey@etu.emse.fr>
2023-04-28 18:32:43 +0200

    clean and comment codes

--- drawHistograms.py
+++ drawHistograms.py
@@ -2,12 +2,13 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-valuesOutputDirectory = "AreaAndPerimeterValues/"
+valuesOutputDirectory = "OutputValues/"
 
 listOfImageNames = os.listdir(valuesOutputDirectory)
 
 areas=[]
 perimeters=[]
+# Read values from input files
 for valueFile in os.listdir(valuesOutputDirectory):
     with open(valuesOutputDirectory+valueFile, 'r') as f:
         fullFile = f.read()[:-2] #remove last \n
@@ -16,6 +17,9 @@ for valueFile in os.listdir(valuesOutputDirectory):
             values=elements.split(";")
             areas.append(values[0])
             perimeters.append(values[1])
+
+
+#Draw histograms
 
 plt.subplot(121,title='areas')
 areas=np.array(areas)
--- preprocessing.py
+++ preprocessing.py
@@ -4,11 +4,16 @@ import matplotlib.pyplot as plt
 import skimage as ski
 import cv2
 
+imageInputDirectory = "InputImages/"
+imageOutputDirectory = "OutputImages/"
+valuesOutputDirectory = "OutputValues/"
+
 def applyCleaningFilter(image,filter):
     filteredImage = image.astype(int)-filter.astype(int) #go into int32 to avoid overflow issues
 
     #remove every negative values created by the filter and replace them by 0
     return np.maximum(filteredImage,np.zeros(filteredImage.shape)).astype(np.uint8)
+
 
 #generate the filter to remove the main noises. This filter is the sum of every images
 def generateFilter(inputDirectory,listImages,imageShape):
@@ -40,11 +45,12 @@ def removeElementsTouchingEdge(image):
 def getAreaAndPerimeters(image):
     areas=[]
     perimeters=[]
+
+    #get all the contours
     contours, hierarchy = cv2.findContours(image, 1, 2)
     imageWithAreaAndPerimeter = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
-    # diplay area on the image
-
+    # display and perimeter on the image
     for i, cnt in enumerate(contours):
         M = cv2.moments(cnt)
         if M['m00'] != 0.0:
@@ -60,20 +66,22 @@ def getAreaAndPerimeters(image):
         cv2.putText(imageWithAreaAndPerimeter, f'Perimeter :{perimeter}', (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6,(0, 255, 0), 2)
     return (areas,perimeters,imageWithAreaAndPerimeter)
 
-imageInputDirectory = "Sous_ensemble_test/"
-imageOutputDirectory = "treatedImage/"
-valuesOutputDirectory = "AreaAndPerimeterValues/"
+#==========Beginning of the script=========================
 
+# Get the name of every image in the inputDirectory
 listOfImageNames = os.listdir(imageInputDirectory)
 imageShape = ski.io.imread(imageInputDirectory+listOfImageNames[0]).shape[0:2]
 
+#For every image process it
 for imageName in listOfImageNames:
-    if(os.path.isfile(valuesOutputDirectory+imageName[:-4]+".csv")): #it the image was already treated, remove it
+
+    # it the image was already processed, skip it
+    if(os.path.isfile(valuesOutputDirectory+imageName[:-4]+".csv")):
         continue
 
-    print("treating ",imageName)
 
-    testImage= ski.io.imread(imageInputDirectory+imageName)[:,:,0] #take only one component
+    print("Processing ",imageName)
+    processedImage= ski.io.imread(imageInputDirectory + imageName)[:, :, 0] #take only one component
 
     #if the luminosity filter in not yet saved, create it
     if(not os.path.isfile("filters/luminosityFilter.bmp")):
@@ -82,25 +90,25 @@ for imageName in listOfImageNames:
     filters = ski.io.imread("filters/luminosityFilter.bmp") #get filter from saved images
 
     #print original image to show evolution
-    plt.subplot(221,title="original image")
-    plt.imshow(testImage,"gray")
+    plt.subplot(221,title="Original image")
+    plt.imshow(processedImage, "gray")
     plt.axis("off")
 
 
     #apply luminosity fiter and equalize image
-    cleanedEqualizedImage = ski.exposure.equalize_hist(applyCleaningFilter(testImage, filters))
-    plt.subplot(222,title='cleaned equalized image')
+    cleanedEqualizedImage = ski.exposure.equalize_hist(applyCleaningFilter(processedImage, filters))
+    plt.subplot(222,title='Filtered equalized image')
     plt.imshow(cleanedEqualizedImage, "gray")
     plt.axis("off")
 
     #remove a bit of noise on the image to have a darker background
     morphedImage = cv2.morphologyEx(cleanedEqualizedImage, cv2.MORPH_OPEN, np.ones((4,4), np.uint8))
-    plt.subplot(223,title="darker background with morph open")
+    plt.subplot(223,title="Darker background with morph open")
     plt.imshow(morphedImage,"gray")
     plt.axis("off")
 
-    # uniformize blobs to avoid noise and bloobs fusions with filter
-    plt.subplot(224,title="blured image")
+    # uniformize aggregates to avoid noise and aggregates when applying the segmentation
+    plt.subplot(224,title="Darker background with Morph Open")
     bluedImage = ski.filters.gaussian(morphedImage,1)
     plt.imshow(bluedImage,"gray")
     plt.axis("off")
@@ -110,48 +118,49 @@ for imageName in listOfImageNames:
     #make the image binary with a local threshold
     threshold=ski.filters.threshold_local(bluedImage,301)
     binaryImage = bluedImage>threshold
-    plt.subplot(221,title="binary image")
-    binaryImage = binaryImage.astype(np.uint8) #transorm it to uint8
+    plt.subplot(221,title="Binary image")
+    binaryImage = binaryImage.astype(np.uint8) #transorm it to uint8 to be a valid cv2 input
     binaryImage[binaryImage==True]=255
     plt.imshow(binaryImage,"gray")
     plt.axis("off")
 
-    #remove small noise
+    #remove single pixel noises from the background
     morphedBinaryImage = cv2.morphologyEx(binaryImage, cv2.MORPH_OPEN, np.ones((20, 20), np.uint8))
-    plt.subplot(222, title="remove noise")
+    plt.subplot(222, title="Morphed binary image")
     plt.imshow(morphedBinaryImage,"gray")
     plt.axis("off")
 
     #smooth image with median filter
     medianImage = ski.filters.median(morphedBinaryImage,ski.morphology.disk(10) )
-    plt.subplot(223,title="smoothed image")
+    plt.subplot(223,title="Smoothed binary image")
     plt.imshow(medianImage,"gray")
     plt.axis("off")
 
-    #remove elements touching edges
+    #remove elements touching the edges
     cleanEdgeImage =removeElementsTouchingEdge(medianImage)
-    plt.subplot(224,title="remove edge elements")
+    plt.subplot(224,title="Remove edge elements image")
     plt.axis("off")
     plt.imshow(cleanEdgeImage,"gray")
 
 
-    #find perimeters, areas and blobs and display it on the image
+    #Find each aggregate, get their perimeters and areas and  display the info on the image
     areas,perimeters,imageWithAreaAndPerimeter=getAreaAndPerimeters(cleanEdgeImage)
 
+    # saved the processed image and its corresponding values in a texte file
     plt.imsave(imageOutputDirectory+imageName,imageWithAreaAndPerimeter)
     with open(valuesOutputDirectory+imageName[:-4]+".csv", 'w') as f:
         for area,perimeter in zip(areas,perimeters):
             f.write(str(area)+";"+str(perimeter)+"\n")
 
     plt.figure()
-    # plt.title("final comparison")
-    plt.subplot(121,title="original image")
-    plt.imshow(testImage,"gray")
+
+    # Display a final comparison
+    plt.subplot(121,title="Original image")
+    plt.imshow(processedImage, "gray")
     plt.axis("off")
 
-    plt.subplot(122,title="treated image")
+    plt.subplot(122,title="Final image")
     plt.imshow(imageWithAreaAndPerimeter,"gray")
     plt.axis("off")
-
 
     # plt.show()
